0871e8b08755f284b7e07a36b1983e888b56bab4
uttamkrshaw <uttamkr5599@gmail.com>
2024-04-05 00:42:33 +0530

    Uploading Final Files

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react"
+import Charts from "react-apexcharts"
+import axios from "axios";
 function App() {
+  const [data1, setData1] = useState()
+  const [data2, setData2] = useState()
+
+  useEffect(() => {
+    axios.get("http://localhost:4500/api/v1/list/data")
+      .then((res) => (setData1(res.data.data1), setData2(res.data.data2)))
+      .catch((err) => console.log(err))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Live Chart For Investment & Profit </h1>
+      <Charts
+        type="bar"
+        width={1000}
+        height={1000}
+        series={
+          [
+            {
+              name: "Investment",
+              data: data1
+            },
+            {
+              name: "Profit",
+              data: data2
+            }
+          ]
+        }
+        options={{
+          theme: {
+            mode: 'dark'
+          },
+          chart: {
+            stacked: true,
+            stackType: 'vertical'
+          },
+          colors: ["#EEEEEE", "#76ABAE"],
+          animations: {
+            enabled: true,
+            easing: 'easeinout',
+            speed: 800,
+            animateGradually: {
+              enabled: true,
+              delay: 1500
+            },
+            dynamicAnimation: {
+              enabled: true,
+              speed: 3500
+            }
+          },
+          fill: {
+            type: 'gradient',
+            gradient: {
+              shade: 'dark',
+              type: "horizontal",
+              shadeIntensity: 0.5,
+              gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+              inverseColors: true,
+              opacityFrom: 1,
+              opacityTo: 1,
+              stops: [0, 50, 100],
+              colorStops: []
+            }
+          },
+        }}
+      >
+
+      </Charts>
+    </div >
   );
 }
 
